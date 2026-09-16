fdff8cba3642c9fef8a5332a9ec8fe4c98423b7d
Ian Walubengo <walubengo.ie.bengo@gmail.com>
2026-09-16 22:53:14 +0300

    Complete Python Lesson 2 - Variables and Data Types

---
+++ lesson_02 .py
@@ -0,0 +1,53 @@
+name = "Ian"
+age = 29
+country = "kenya"
+print(name)
+print(age)
+print(country)
+print(type(name))
+print(type(age))
+print(type(country))
+
+salary = 50000
+has_job = True
+profession = "Accountant"
+print(salary)
+print(has_job)
+print(profession)
+print(type(salary))
+print(type(has_job))
+print(type(profession))
+annual_salary = salary * 12
+print(annual_salary)
+monthly_expenses = 30000
+annual_expenses = monthly_expenses * 12
+annual_savings = annual_salary - annual_expenses
+print(annual_salary)
+print(annual_expenses)
+print(annual_savings)
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+print(10 % 3)
+salary = int(input("What is your monthly salary? "))
+print(salary)
+print(type(salary))
+salary = int(input("What is your monthly salary? "))
+expenses = int(input("What are your monthly expenses? "))
+salary = int(input("What is your monthly salary? "))
+expenses = int(input("What are your monthly expenses? "))
+
+monthly_savings = salary - expenses
+annual_salary = salary * 12
+annual_savings = monthly_savings * 12
+
+print("Monthly savings:", monthly_savings)
+print("Annual salary:", annual_salary)
+print("Annual savings:", annual_savings)
+print(salary > expenses)
+print(salary == expenses)
+print(salary < expenses)
+print(salary > expenses)
+print(salary == expenses)
+print(salary < expenses)
